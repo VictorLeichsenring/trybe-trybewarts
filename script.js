0c7dcba491ba5249c7d554a9ecfb383fe9e78b28
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-24 21:16:06 -0300

    Codicionamento do botão enviar ao agreement

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btnEnviarHeader = document.getElementById('btn-enviar-header');
 const inputEmailHeader = document.getElementById('input-email-header');
 const inputSenhaHeader = document.getElementById('input-password-header');
+const checkboxAgreement = document.getElementById('agreement');
+const btnSubmitForm = document.getElementById('submit-btn');
 
 function login() {
   if (inputEmailHeader.value === 'tryber@teste.com' && inputSenhaHeader.value === '123456') {
@@ -10,4 +12,13 @@ function login() {
   }
 }
 
+function agreementChange() {
+  if (checkboxAgreement.checked) {
+    btnSubmitForm.disabled = false;
+  } else {
+    btnSubmitForm.disabled = true;
+  }
+}
+
 btnEnviarHeader.addEventListener('click', login);
+checkboxAgreement.addEventListener('change', agreementChange);
